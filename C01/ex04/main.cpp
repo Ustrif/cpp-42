79b4be88effb57692fe89e6f2691df0528efc787
Resul Aydoğmuş <39741608+Ustrif@users.noreply.github.com>
2025-08-11 13:13:57 +0000

    ex04 is done.

--- C01/ex04/main.cpp
+++ C01/ex04/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 int	main(int argc, char *argv[])
 {
@@ -8,5 +9,38 @@ int	main(int argc, char *argv[])
 		std::cout << "Invalid arguments.\n";
 		return (1);
 	}
-	return 0;
+	std::string	infileName = argv[1];
+	std::string	outfileName = infileName + ".replace";
+
+	std::string	willFind = argv[2];
+	std::string	willWrite = argv[3];
+
+	std::ofstream fileWrite(outfileName.c_str());
+	bool	firstLine = true;
+	if (fileWrite.is_open())
+	{
+		std::ifstream fileRead(infileName.c_str());
+		if (fileRead.is_open())
+		{
+			std::string	s;
+			while (getline(fileRead, s))
+			{
+					if (!firstLine)
+						fileWrite << "\n";
+					else
+						firstLine = false;
+				size_t findIn = 0;
+				while ((findIn = s.find(willFind, findIn)) != std::string::npos)
+				{
+					s.erase(findIn, willFind.length());
+					s.insert(findIn, willWrite);
+					findIn += willWrite.length();
+				}
+				fileWrite << s;
+			}
+			fileRead.close();
+		}
+		fileWrite.close();
+	}
+	return (0);
 }
