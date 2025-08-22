4940c1f60b047204bd0dd832f35ba669a8cf73b3
Resul Aydoğmuş <39741608+Ustrif@users.noreply.github.com>
2025-08-22 07:50:18 +0000

    C02, ex01 is done.

---
+++ C02/ex01/Fixed.hpp
@@ -0,0 +1,19 @@
+#ifndef __FIXED__
+#define __FIXED__
+
+class Fixed
+{
+	private:
+		int	fpVal;
+		static const int fractionalBits = 8;
+	public:
+		Fixed();
+		Fixed(int number);
+		Fixed(const Fixed &other);
+		Fixed &operator=(const Fixed &other);
+		~Fixed();
+		int getRawBits( void ) const;
+		void setRawBits( int const raw );
+};
+
+#endif //__FIXED__
