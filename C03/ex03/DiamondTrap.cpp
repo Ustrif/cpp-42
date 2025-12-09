1233edf4303db9337cc1c382838c43348f591fb2
Ustrif <39741608+Ustrif@users.noreply.github.com>
2025-12-09 22:27:43 +0300

    diamond trap is edited.

--- C03/ex03/DiamondTrap.cpp
+++ C03/ex03/DiamondTrap.cpp
@@ -27,70 +27,26 @@ DiamondTrap::~DiamondTrap()
 	std::cout << "DiamondTrap destructor\n";
 }
 
-DiamondTrap::DiamondTrap(const DiamondTrap &other) : ClapTrap(other.getName()), FragTrap(other.getName()), ScavTrap(other.getName())
+DiamondTrap::DiamondTrap(const DiamondTrap &other) : ClapTrap(other.name), FragTrap(other.name), ScavTrap(other.name)
 {
 	ClapTrap::name.append("_clap_name");
-	DiamondTrap::name = other.getName();
-	DiamondTrap::attackDamage = other.getAttackDamage();
-	DiamondTrap::hitPoints = other.getHitPoints();
-	DiamondTrap::energyPoints = other.getEnergyPoints();
+	DiamondTrap::name = other.name;
+	DiamondTrap::attackDamage = other.attackDamage;
+	DiamondTrap::hitPoints = other.hitPoints;
+	DiamondTrap::energyPoints = other.energyPoints;
 	std::cout << "DiamondTrap copy constructor\n";
 }
 
 DiamondTrap & DiamondTrap::operator = (const DiamondTrap &other)
 {
-	ClapTrap::name = other.getName();
+	ClapTrap::name = other.name;
 	ClapTrap::name.append("_clap_name");
-	DiamondTrap::name = other.getName();
-	DiamondTrap::attackDamage = other.getAttackDamage();
-	DiamondTrap::hitPoints = other.getHitPoints();
-	DiamondTrap::energyPoints = other.getEnergyPoints();
+	DiamondTrap::name = other.name;
+	DiamondTrap::attackDamage = other.attackDamage;
+	DiamondTrap::hitPoints = other.hitPoints;
+	DiamondTrap::energyPoints = other.energyPoints;
 	std::cout << "DiamondTrap copy assigment operator\n";
 	return (*this);
-}
-
-void	DiamondTrap::attack(const std::string& target)
-{
-	ScavTrap::attack(target);
-	std::cout << "DiamondTrap " << DiamondTrap::name << " attack with ScavTrap side\n";
-}
-
-void	DiamondTrap::takeDamage(unsigned int amount)
-{
-	if (this->hitPoints < amount)
-	{
-		this->hitPoints = 0;
-		std::cout << "DiamondTrap " << DiamondTrap::name << " died.\n";
-	}
-	else
-	{
-		this->hitPoints -= amount;
-		std::cout << "DiamondTrap " << DiamondTrap::name << " took damage " << amount << " points.\n";
-	}
-}
-
-void	DiamondTrap::beRepaired(unsigned int amount)
-{
-	if (this->energyPoints > 0 || this->hitPoints == 0)
-	{
-		this->hitPoints += amount;
-		this->energyPoints--;
-	}
-	else
-	{
-		std::cout << "No energy points.\n";
-		return ;
-	}
-	std::cout	<< "DiamondTrap "
-				<< this->name
-				<< " is reapired: "
-				<< amount
-				<< ".\n";
-}
-
-std::string	DiamondTrap::getName( void ) const
-{
-	return (DiamondTrap::name);
 }
 
 void DiamondTrap::whoAmI()
@@ -98,46 +54,11 @@ void DiamondTrap::whoAmI()
 	std::cout << "DiamondTrap Name: " << DiamondTrap::name << " ClapTrap Name: " << ClapTrap::name << std::endl;
 }
 
-void DiamondTrap::setHitPoints(unsigned int amount)
-{
-	this->hitPoints = amount;
-}
-
-void DiamondTrap::setEnergyPoints(unsigned int amount)
-{
-	this->energyPoints = amount;
-}
-
-void DiamondTrap::setAttackDamage(unsigned int amount)
-{
-	this->attackDamage = amount;
-}
-
-unsigned int DiamondTrap::getHitPoints() const
-{
-	return (this->hitPoints);
-}
-
-unsigned int DiamondTrap::getEnergyPoints() const
-{
-	return (this->energyPoints);
-}
-
-unsigned int DiamondTrap::getAttackDamage() const
-{
-	return (this->attackDamage);
-}
-
-void DiamondTrap::setName(std::string name)
-{
-	this->name = name;
-}
-
 void DiamondTrap::showStatics( void ) const
 {
 	std::cout << "Statics: "
-	<< "Attack Damage: " << this->getAttackDamage()
-	<< " Hit Points: " << this->getHitPoints()
-	<< " Energy Points: " << this->getEnergyPoints()
+	<< "Attack Damage: " << this->attackDamage
+	<< " Hit Points: " << this->hitPoints
+	<< " Energy Points: " << this->energyPoints
 	<< std::endl; 
 }
--- C03/ex03/DiamondTrap.hpp
+++ C03/ex03/DiamondTrap.hpp
@@ -15,21 +15,8 @@ class DiamondTrap : public FragTrap, public ScavTrap
 		~DiamondTrap();
 		DiamondTrap(const DiamondTrap &other);
 		DiamondTrap &operator=(const DiamondTrap &other);
-		void attack(const std::string& target);
-		void takeDamage(unsigned int amount);
-		void beRepaired(unsigned int amount);
 		void showStatics( void ) const;
 		void whoAmI();
-	protected:
-		std::string	getName( void ) const;
-		void setHitPoints(unsigned int amount);
-		void setEnergyPoints(unsigned int amount);
-		void setAttackDamage(unsigned int amount);
-		void setName(std::string name);
-		unsigned int getHitPoints() const;
-		unsigned int getEnergyPoints() const;
-		unsigned int getAttackDamage() const;
-
 };
 
 #endif //__DIAMOND__
