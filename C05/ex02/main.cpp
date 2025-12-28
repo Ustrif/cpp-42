1e3176ae1d7722f6a88aeaef9bf27cc9f5a76769
Ustrif <39741608+Ustrif@users.noreply.github.com>
2025-12-28 21:28:27 +0300

    main.cpp done.

--- C05/ex02/main.cpp
+++ C05/ex02/main.cpp
@@ -1,21 +1,36 @@
-#include "Bureaucrat.hpp"
 #include <iostream>
-#include "Form.hpp"
 #include <string>
+#include "Bureaucrat.hpp"
+#include "AForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
 
-void	tester(std::string str, int sing, int exec, Bureaucrat *br)
+void	tester(std::string str, Bureaucrat *br, int formtype)
 {
-	Form	*form;
-	
+	AForm	*form;
+
 	std::cout << "---" << std::endl;
 	try
 	{
-		form = new Form(str, sing, exec);
+		switch (formtype)
+		{
+		case 1:
+			form = new PresidentialPardonForm(str);
+			break;
+		case 2:
+			form = new RobotomyRequestForm(str);
+			break;
+		default:
+			form = new ShrubberyCreationForm(str);
+			break;
+		}
 	}
 	catch(const std::exception& e)
 	{
 		std::cout << "ERROR TO CREATE FORM: ";
 		std::cout << e.what();
+		std::cout << "---" << std::endl;
 		return ;
 	}
 	try
@@ -27,19 +42,68 @@ void	tester(std::string str, int sing, int exec, Bureaucrat *br)
 	{
 		std::cout << e.what() << '\n';
 	}
+	try
+	{
+		std::cout << "\n<<<>>>\n" << std::endl;
+		br->executeForm(*form);
+		std::cout << "\n<<<>>>\n" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
 	std::cout << *form << std::endl;
 	delete(form);
-	std::cout << "---" << std::endl;	
+	std::cout << "---" << std::endl;
+}
+
+void	file_test( void )
+{
+	std::cout << std::endl << "FILE TEST BEGIN" << std::endl;
+
+	AForm* f;
+	try
+	{
+		Bureaucrat a("1LVL", 1);
+		f = new ShrubberyCreationForm("/root/forbidden_file");
+		f->beSigned(a);
+		f->execute(a);
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	
+	delete(f);
+	std::cout << std::endl << "FILE TEST END" << std::endl;
 }
 
 int	main ( void )
 {
 	Bureaucrat *br = new Bureaucrat("20LVL", 20);
-	tester("FORM1", 20, 20, br);
-	tester("FORM2", 5, 5, br);
-	tester("FORM3", 100, 100, br);
-	tester("FORMUP", 200, 200, br);
-	tester("FORMDOWN", -1, -1, br);
+	tester("20 - FORM1", br, 1);
+	tester("20 - FORM2", br, 2);
+	tester("20 - FORM3", br, 3);
 	delete(br);
+
+	std::cout << "\n***\n";
+
+	br = new Bureaucrat("150LVL", 150);
+	tester("150 - FORM1", br, 1);
+	tester("150 - FORM2", br, 2);
+	tester("150 - FORM3", br, 3);
+	delete(br);
+
+	std::cout << "\n***\n";
+
+	br = new Bureaucrat("1LVL", 1);
+	tester("1 - FORM1", br, 1);
+	tester("1 - FORM2", br, 2);
+	tester("1 - FORM3", br, 3);
+	delete(br);
+
+	file_test();
+
 	return (0);
 }
