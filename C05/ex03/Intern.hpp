7ef72b3417f67efaf1f0a326488fb120ec38ac17
Ustrif <39741608+Ustrif@users.noreply.github.com>
2025-12-28 22:13:36 +0300

    Intern.hpp is completely done.

--- C05/ex03/Intern.hpp
+++ C05/ex03/Intern.hpp
@@ -1,17 +1,39 @@
 #ifndef __INTERN__
 #define __INTERN__
 
+#define FORM_NUM 3
+
 #include "AForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
 
 class Intern
 {
+	private:
+		int	number;
 	public:
 		Intern();
 		~Intern();
 		Intern(const Intern& other);
 		Intern &operator = (const Intern& other);
 
+		int		getNumber( void ) const;
+		void	setNumber( int num );
+
 		AForm* makeForm(std::string formName, std::string formTarget);
+
+		class NoNameError : public std::exception
+		{
+			public:
+				const char* what() const throw();
+		};
+		class UndefinedNameError : public std::exception
+		{
+			public:
+				const char* what() const throw();
+		};
+
 };
 
 #endif // __INTERN__
