4748c79c3acfcd0ef999e5eb4c8fa9fe5a2a6e98
Ustrif <39741608+Ustrif@users.noreply.github.com>
2025-12-28 22:11:16 +0300

    too many new lines removed from ex03

--- C05/ex03/PresidentialPardonForm.cpp
+++ C05/ex03/PresidentialPardonForm.cpp
@@ -2,18 +2,18 @@
 
 PresidentialPardonForm::PresidentialPardonForm(std::string target) : AForm(target, 25, 5)
 {
-	std::cout << "Presidential Pardon form constructor\n" << std::endl;
+	std::cout << "Presidential Pardon form constructor" << std::endl;
 }
 
 PresidentialPardonForm::~PresidentialPardonForm()
 {
-	std::cout << "Presidential Pardon form destructor\n" << std::endl;
+	std::cout << "Presidential Pardon form destructor" << std::endl;
 }
 
 PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm& other) 
 : AForm(other.getName(), other.getGradeSign(), other.getGradeExecute())
 {
-	std::cout << "Presidential Pardon form copy constructor\n" << std::endl;
+	std::cout << "Presidential Pardon form copy constructor" << std::endl;
 }
 
 PresidentialPardonForm &PresidentialPardonForm::operator = (const PresidentialPardonForm& other)
@@ -21,7 +21,7 @@ PresidentialPardonForm &PresidentialPardonForm::operator = (const PresidentialPa
 	if (this != &other)
 		AForm::operator=(other);
 
-	std::cout << "Presidential Pardon form copy assigment operator\n" << std::endl;
+	std::cout << "Presidential Pardon form copy assigment operator" << std::endl;
 	return (*this);
 }
 
