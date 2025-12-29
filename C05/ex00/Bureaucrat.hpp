3dcdbad7d31a38a281992c6917fbf41a1e991e99
Ustrif <39741608+Ustrif@users.noreply.github.com>
2025-12-29 17:11:55 +0300

    C05/ex00 virtual keyword added to Exception and new line added.

--- C05/ex00/Bureaucrat.hpp
+++ C05/ex00/Bureaucrat.hpp
@@ -25,12 +25,12 @@ class Bureaucrat
 		class GradeTooHighException : public std::exception
 		{
 			public:
-				const char* what() const throw();
+				virtual const char* what() const throw();
 		};
 		class GradeTooLowException : public std::exception
 		{
 			public:
-				const char* what() const throw();
+				virtual const char* what() const throw();
 		};
 };
 
