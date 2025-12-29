16c4cad8c516270b9d0a7cf5cb3456b89711a13b
Ustrif <39741608+Ustrif@users.noreply.github.com>
2025-12-29 17:08:27 +0300

    C05/ex03 full main.cpp, no leaks.

--- C05/ex03/main.cpp
+++ C05/ex03/main.cpp
@@ -1,109 +1,38 @@
 #include <iostream>
 #include <string>
-#include "Bureaucrat.hpp"
 #include "AForm.hpp"
 #include "PresidentialPardonForm.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "ShrubberyCreationForm.hpp"
+#include "Intern.hpp"
 
-void	tester(std::string str, Bureaucrat *br, int formtype)
+void	test_cpp(Intern& intern, std::string formName, std::string formTarget)
 {
-	AForm	*form;
+	AForm* rrf;
 
-	std::cout << "---" << std::endl;
 	try
 	{
-		switch (formtype)
-		{
-		case 1:
-			form = new PresidentialPardonForm(str);
-			break;
-		case 2:
-			form = new RobotomyRequestForm(str);
-			break;
-		default:
-			form = new ShrubberyCreationForm(str);
-			break;
-		}
-	}
-	catch(const std::exception& e)
-	{
-		std::cout << "ERROR TO CREATE FORM: ";
-		std::cout << e.what();
-		std::cout << "---" << std::endl;
-		return ;
-	}
-	try
-	{
-		std::cout << *form << std::endl;
-		br->signForm(*form);
-	}
-	catch (const std::exception& e)
-	{
-		std::cout << e.what() << '\n';
-	}
-	try
-	{
-		std::cout << "\n<<<>>>\n" << std::endl;
-		br->executeForm(*form);
-		std::cout << "\n<<<>>>\n" << std::endl;
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
-	
-	std::cout << *form << std::endl;
-	delete(form);
-	std::cout << "---" << std::endl;
-}
-
-void	file_test( void )
-{
-	std::cout << std::endl << "FILE TEST BEGIN" << std::endl;
-
-	AForm* f;
-	try
-	{
-		Bureaucrat a("1LVL", 1);
-		f = new ShrubberyCreationForm("/root/forbidden_file");
-		f->beSigned(a);
-		f->execute(a);
+		rrf = intern.makeForm(formName, formTarget);
+		std::cout << *rrf << std::endl;
+		delete (rrf);
 	}
 	catch (std::exception& e)
 	{
-		std::cout << e.what() << std::endl;
+		std::cout << "Intern couldn't create a Form because " << e.what() << std::endl;
 	}
-	
-	delete(f);
-	std::cout << std::endl << "FILE TEST END" << std::endl;
+
+	std::cout << "***" << std::endl;
 }
 
 int	main ( void )
 {
-	Bureaucrat *br = new Bureaucrat("20LVL", 20);
-	tester("20 - FORM1", br, 1);
-	tester("20 - FORM2", br, 2);
-	tester("20 - FORM3", br, 3);
-	delete(br);
+	Intern someRandomIntern;
 
-	std::cout << "\n***\n";
-
-	br = new Bureaucrat("150LVL", 150);
-	tester("150 - FORM1", br, 1);
-	tester("150 - FORM2", br, 2);
-	tester("150 - FORM3", br, 3);
-	delete(br);
-
-	std::cout << "\n***\n";
-
-	br = new Bureaucrat("1LVL", 1);
-	tester("1 - FORM1", br, 1);
-	tester("1 - FORM2", br, 2);
-	tester("1 - FORM3", br, 3);
-	delete(br);
-
-	file_test();
+	test_cpp(someRandomIntern, "robotomy request", "Bender");
+	test_cpp(someRandomIntern, "presidential", "X");
+	test_cpp(someRandomIntern, "ShrubberyCreationForm", "Y");
+	test_cpp(someRandomIntern, "", "");
+	test_cpp(someRandomIntern, "ax", "ax");
 
 	return (0);
 }
