21f4f350dc1078507fe09c0ec86765ee4f08e2ac
Ustrif <39741608+Ustrif@users.noreply.github.com>
2026-04-04 23:02:19 +0300

    8/ex00 is done.

--- C08/ex00/easyfind.hpp
+++ C08/ex00/easyfind.hpp
@@ -1,6 +1,15 @@
 #ifndef	EASY_FIND
 #define EASY_FIND
 
+#include <algorithm>
 
+template<typename T>
+typename T::iterator	easyfind(T& dataset, const int num)
+{
+	typename T::iterator	result;
+
+	result = std::find(dataset.begin(), dataset.end(), num);
+	return (result);
+}
 
 #endif // EASY_FIND
--- C08/ex00/main.cpp
+++ C08/ex00/main.cpp
@@ -1,8 +1,39 @@
 #include "easyfind.hpp"
 #include <iostream>
+#include <vector>
+
+#define VAL 21
+#define VAL2 3
 
 int	main (void)
 {
+	std::vector<int> vector;
 	
+	vector.push_back(20);
+	vector.push_back(3);
+	vector.push_back(17);
+	vector.push_back(100);
+	vector.push_back(80);
+	vector.push_back(1);
+
+	std::cout << "Try to find: " << VAL << std::endl;
+	std::vector<int>::iterator result = easyfind(vector, VAL);
+
+	if (vector.end() == result)
+		std::cout << "There is nothing." << std::endl;
+
+	std::cout << "Try to find: " << VAL2 << std::endl;
+	std::vector<int>::iterator results = easyfind(vector, VAL2);
+
+	if (vector.end() == results)
+		std::cout << "There is nothing." << std::endl;
+
+	std::cout << "Remains:" << std::endl;
+	while (vector.end() != results)
+	{
+		std::cout << *results << std::endl;
+		results++;
+	}
+
 	return (0);
 }
